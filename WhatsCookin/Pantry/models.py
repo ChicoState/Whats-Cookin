e36d032e2a81b3df1e33d70a68138f2ffebfd613
TheZipLord <78382950+TheZipLord@users.noreply.github.com>
2022-05-10 23:37:15 -0700

    commit to resolve stash issues

--- WhatsCookin/Pantry/models.py
+++ WhatsCookin/Pantry/models.py
@@ -1,3 +1,29 @@
+from tkinter import TRUE
 from django.db import models
 
 # Create your models here.
+# A modle is a class that represents a database table 
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100, null = True)
+    phone = models.CharField(max_length=100, null = True)
+    email = models.CharField(max_length=100, null = True)
+    date_created = models.DateTimeField(auto_now_add=True, null = True)
+
+    def __str__(self):
+        return self.name
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100, null = True)
+
+    def __str__(self):
+        return self.name
+
+class Fridge(models.Model):
+    name = models.CharField(max_length=100, null = True, blank = True)
+    date_stocked = models.DateTimeField(auto_now_add=True, null = True)
+    customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
+    ingredient = models.ManyToManyField(Ingredient, blank = True)
+
+    def __str__(self):
+        return self.name
--- WhatsCookin/Pantry/views.py
+++ WhatsCookin/Pantry/views.py
@@ -1,6 +1,61 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect, HttpResponse
-# Create your views here.
+from django.contrib.auth.decorators import login_required
 
+from .models import *
+from .forms import FridgeForm
+
+# Create your views here.
+# Views define how the html page will render and look. they use data from the model
+@login_required(login_url='login')
 def Pantry(request):
-    return render(request,"Pantry/Pantry.html")
+    return render(request, "Pantry/Pantry.html")
+
+@login_required(login_url='login')
+def dashboard(request, pri_key):
+    customer = Customer.objects.get(id=pri_key)
+
+    fridges = customer.fridge_set.all() # related set 
+    fridge_count = fridges.count()
+
+    ingredient_count = customer.fridge_set.first().ingredient.count() #only working with one fridge
+
+    context = {'customer':customer, 'fridges':fridges, 'fridge_count':fridge_count, 'ingredient_count':ingredient_count}
+    return render(request, 'pantry/dashboard.html', context)
+
+@login_required(login_url='login')
+def createFridge(request):
+
+    form = FridgeForm()
+    if request.method == 'POST':
+        form = FridgeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('pantry/dashboard.html')
+
+    context = {'form':form}
+    return render(request, 'pantry/createFridge.html', context)
+
+@login_required(login_url='login')
+def updateFridge(request, cust_key, fri_key):
+
+    fridge = Fridge.objects.get(id=fri_key)
+    form = FridgeForm(instance=fridge)
+
+    if request.method == 'POST':
+        form = FridgeForm(request.POST, instance=fridge)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard', pri_key=cust_key)
+
+    context = {'form':form}
+    return render(request, 'pantry/updateFridge.html', context)
+
+@login_required(login_url='login')
+def deleteFridge(request, cust_key, fri_key):
+	fridge = Fridge.objects.get(id=pri_key)
+	if request.method == "POST":
+		fridge.delete()
+		return redirect('dashboard', pri_key=cust_key)
+
+	context = {'item':fridge}
+	return render(request, 'pantry/deleteFridge.html', context)
