a87ccffe85cac06bb49fc8f5af400d2a852b2bfe
bserrato7900 <baserrato@mail.csuchico.edu>
2022-05-13 20:20:28 -0700

    Saving changes before pantry

--- accounts/views.py
+++ accounts/views.py
@@ -48,7 +48,7 @@ def loginPage(request):
 
 def logoutUser(request):
     logout(request)
-    return redirect('dashboard')
+    return redirect('login')
 
 @login_required(login_url='login')
 def dashboard(request):
